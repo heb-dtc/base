c5dbc9b08cb8cb74cd697da9d98a65ad01c54051
Florent NOEL <florent.noel@protonmail.com>
2020-04-02 13:42:26 +0200

    add screens script configuration for linux

--- scripts/screens.sh
+++ scripts/screens.sh
@@ -2,25 +2,64 @@
 intern=eDP1
 extern=DP1
 
-if [ $# -ne 1 ]; then
-    echo "Usage: provide the wanted screen setup (intern|extern|dual)"
-    echo "Example: screens intern|extern|dual"
-    exit 1
-fi
+usage() {
+    local programName
+    programName=${0##*/}
+    cat <<EOF
+Usage: $programName [-option]
 
-mode=$1
+Options:
+    --help Print this message
+    -dual Dual screen setup
+    -extern Only external screen setup
+    -intern Only internal screen setup
+    -frgb Apply full rgb mode to external screen to have real blacks
+EOF
+}   
 
-if [ "$mode" == "extern" ]; then
-    if xrandr | grep "$extern disconnected"; then
-        echo "external screen not detected. Is it plugged?"
-        exit 1
+setupScreen() {
+    mode=$1
+
+    if [ "$mode" == "extern" ]; then
+        if xrandr | grep "$extern disconnected"; then
+            echo "external screen not detected. Is it plugged?"
+            exit 1
+        fi
+        xrandr --output "$intern" --off --output "$extern" --auto
+    elif [ "$mode" == "intern" ]; then
+        xrandr --output "$extern" --off --output "$intern" --auto
+    elif [ "$mode" == "dual" ]; then
+        xrandr --output "$extern" --above "$intern" --auto
     fi
-    xrandr --output "$intern" --off --output "$extern" --auto
-elif [ "$mode" == "intern" ]; then
-    xrandr --output "$extern" --off --output "$intern" --auto
-elif [ "$mode" == "dual" ]; then
-    xrandr --output "$extern" --above "$intern" --auto
-elif [ "$mode" == "set-real-black" ]; then
-    xrandr --output DP1 --set "Broadcast RGB" "Full"
-fi
+}
 
+fullRgb() {
+    xrandr --output $extern --set "Broadcast RGB" "Full"
+}
+
+main() {
+    case "$1" in 
+        ''|-h|--help)
+            usage
+            exit 0
+            ;;
+    -dual)
+        setupScreen dual
+        ;;
+    -intern)
+        setupScreen intern
+        ;;
+    -extern)
+        setupScreen extern
+        ;;
+    -frgb)
+        fullRgb
+        ;;
+    *)
+        echo "Command not found"
+        exit 1
+        ;;
+    esac
+}
+
+main "$@"
