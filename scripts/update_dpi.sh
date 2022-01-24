65a8552475e7d97a2818c758bfb1ebb97307529e
Florent NOEL <florent.noel@protonmail.com>
2022-01-24 09:44:32 +0100

    Update dpi script fixup

--- scripts/update_dpi.sh
+++ scripts/update_dpi.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 # change the DPI settings in Xresources
 
-if [ "$AUTORANDR_CURRENT_PROFILE" = "dock" ]
-then
-  sed -i -e '$s/120/96/' /home/flow/base/dotFiles/i3/Xresources
-  notify-send -i display "Display profile" "$AUTORANDR_CURRENT_PROFILE - 96DPI"
-elif [ "$AUTORANDR_CURRENT_PROFILE" = "laptop" ]
+if [ "$AUTORANDR_CURRENT_PROFILE" = "laptop" ]
 then
   sed -i -e '$s/96/120/' /home/flow/base/dotFiles/i3/Xresources
   notify-send -i display "Display profile" "$AUTORANDR_CURRENT_PROFILE - 120DPI"
 else
-  notify-send -i display "Display profile" "$AUTORANDR_CURRENT_PROFILE unknown"
+  sed -i -e '$s/120/96/' /home/flow/base/dotFiles/i3/Xresources
+  notify-send -i display "Display profile" "$AUTORANDR_CURRENT_PROFILE - 96DPI"
 fi
 
 # reload Xresource
