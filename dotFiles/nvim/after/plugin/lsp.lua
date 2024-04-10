c079b3db23178414437eadbf27a7924e3be4e802
Florent NOEL <florent.noel@protonmail.com>
2024-04-10 18:35:49 +0200

    Update nvim setup

--- dotFiles/nvim/after/plugin/lsp.lua
+++ dotFiles/nvim/after/plugin/lsp.lua
@@ -1,5 +1,12 @@
 local lsp = require("lsp-zero")
 
+lsp.set_sign_icons({
+	error = "✘",
+	warn = "▲",
+	hint = "⚑",
+	info = "»",
+})
+
 require("mason").setup({})
 require("mason-lspconfig").setup({
 	ensure_installed = { "tsserver", "eslint" },
@@ -9,6 +16,13 @@ require("mason-lspconfig").setup({
 			local lua_opts = lsp.nvim_lua_ls()
 			require("lspconfig").lua_ls.setup(lua_opts)
 		end,
+		clangd = function()
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities.offsetEncoding = { "utf-16" }
+			require("lspconfig").clangd.setup({
+				capabilities = capabilities,
+			})
+		end,
 	},
 })
 
@@ -16,6 +30,7 @@ require("mason-lspconfig").setup({
 -- lsp.nvim_workspace()
 
 local cmp = require("cmp")
+local cmp_action = lsp.cmp_action()
 
 require("luasnip.loaders.from_vscode").lazy_load()
 
@@ -28,6 +43,10 @@ cmp.setup({
 	mapping = cmp.mapping.preset.insert({
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
 		["<C-e>"] = cmp.mapping.abort(),
+		["<C-u>"] = cmp.mapping.scroll_docs(-4),
+		["<C-d>"] = cmp.mapping.scroll_docs(4),
+		["<C-f>"] = cmp_action.luasnip_jump_forward(),
+		["<C-b>"] = cmp_action.luasnip_jump_backward(),
 		["<C-Space>"] = cmp.mapping.complete(),
 	}),
 	window = {
@@ -43,33 +62,56 @@ lsp.on_attach(function(client, bufnr)
 	vim.keymap.set("n", "gd", function()
 		vim.lsp.buf.definition()
 	end, opts)
+
+	vim.keymap.set("n", "gD", function()
+		vim.lsp.buf.declaration()
+	end, opts)
+
+	vim.keymap.set("n", "go", function()
+		vim.lsp.buf.implementation()
+	end, opts)
+
+	vim.keymap.set("n", "gs", function()
+		vim.lsp.buf.signature_help()
+	end, opts)
+
 	vim.keymap.set("n", "K", function()
 		vim.lsp.buf.hover()
 	end, opts)
+
 	vim.keymap.set("n", "<leader>ws", function()
 		vim.lsp.buf.workspace_symbol()
 	end, opts)
+
 	vim.keymap.set("n", "<leader>d", function()
 		vim.diagnostic.open_float()
 	end, opts)
+
+	vim.keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+	--vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+	vim.keymap.set("n", "<leader>T", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+
 	vim.keymap.set("n", "[d", function()
 		vim.diagnostic.goto_next()
 	end, opts)
+
 	vim.keymap.set("n", "]d", function()
 		vim.diagnostic.goto_prev()
 	end, opts)
+
 	vim.keymap.set("n", "<leader>ca", function()
 		vim.lsp.buf.code_action()
 	end, opts)
+
 	vim.keymap.set("n", "<leader>rr", function()
 		vim.lsp.buf.references()
 	end, opts)
+
 	vim.keymap.set("n", "<leader>rn", function()
 		vim.lsp.buf.rename()
 	end, opts)
-	vim.keymap.set("i", "<C-h>", function()
-		vim.lsp.buf.signature_help()
-	end, opts)
+
+	vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
 end)
 
 lsp.setup()
--- dotFiles/nvim/after/plugin/null-ls.lua
+++ dotFiles/nvim/after/plugin/null-ls.lua
@@ -3,21 +3,21 @@ local b = null_ls.builtins
 
 local sources = {
 	b.formatting.stylua,
-	b.diagnostics.luacheck.with({
-		extra_args = { "--globals", "vim", "describe" },
-	}),
+	-- b.diagnostics.luacheck.with({
+	-- 	extra_args = { "--globals", "vim", "describe" },
+	-- }),
 	b.formatting.prettier,
-	b.diagnostics.eslint_d,
+	-- b.diagnostics.eslint_d,
 	-- b.formatting.eslint_d,
-	b.code_actions.eslint_d,
+	-- b.code_actions.eslint_d,
 	b.completion.spell,
 	b.formatting.shfmt.with({ extra_args = { "-i", "2", "-ci" } }),
-	b.diagnostics.shellcheck,
+	-- b.diagnostics.shellcheck,
 	b.diagnostics.vint,
 	b.diagnostics.yamllint,
 	b.formatting.gofumpt,
-	b.formatting.clang_format,
-	b.diagnostics.cppcheck,
+	--b.formatting.clang_format,
+	--b.diagnostics.cppcheck,
 }
 
 null_ls.setup({
