cbe2b0a5e77c0b1cc5a58e257bf897d700fd2550
Florent NOEL <florent.noel@protonmail.com>
2024-05-02 21:19:49 +0200

    Udpate

---
+++ server/backups/immich-daily-bkp.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+IMMICH_DIR=/home/flow/apps/immich
+LOCAL_BKP_DIR=/home/flow/bkp/immich
+CUBBIT_REMOTE=cubbit-crypted
+B2_REMOTE=b2-crypted
+
+echo "Starting immich daily backup at: $(date)"
+
+# DB dump
+docker exec -t immich_postgres pg_dumpall -c -U postgres | gzip > "$LOCAL_BKP_DIR/dump.sql.gz"
+
+if [ $? -eq 0 ]; then
+  # copy DB dump to cubbit
+  /usr/bin/rclone -v sync $LOCAL_BKP_DIR/dump.sql.gz $CUBBIT_REMOTE:db --progress 
+
+  if [ $? -eq 0 ]; then
+    # copy upload dir/ to cubbit
+    /usr/bin/rclone -v sync $IMMICH_DIR/library/upload/ $CUBBIT_REMOTE:upload --progress
+    if [ $? -ne 0 ]; then
+      echo "Failed to copy photos to cubbit." >&2
+      exit 1
+    fi
+  else
+      echo "Failed to copy DB to cubbit." >&2
+      exit 1
+  fi
+
+  echo "backup to cubbit successful"
+
+  # copy DB dump to b2
+  /usr/bin/rclone -v sync $LOCAL_BKP_DIR/dump.sql.gz $B2_REMOTE:db --progress 
+
+  if [ $? -eq 0 ]; then
+    # copy upload dir/ to b2
+    /usr/bin/rclone -v sync $IMMICH_DIR/library/upload/ $B2_REMOTE:upload --progress
+    if [ $? -ne 0 ]; then
+      echo "Failed to copy photos to b2." >&2
+      exit 1
+    fi
+  else
+      echo "Failed to copy DB to b2." >&2
+      exit 1
+  fi
+
+  echo "backup to b2 successful"
+
+else
+  echo "The database dump failed." >&2
+  exit 1
+fi
+
+echo "immich daily backup completed successfully at: $(date)"
+
+
